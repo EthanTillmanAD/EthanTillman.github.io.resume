8b644190d7880487d3a537c76a977c3ac07cba70
EthanTillmanAD <ethantillman101@gmail.com>
2024-11-18 11:35:48 -0500

    resume-website-testing

---
+++ script.js
@@ -0,0 +1,28 @@
+setInterval(time, 1000);
+function time(){
+    const date  = new Date();
+
+
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+
+    hours = hours ? hours :12;
+    document.getElementById("clock").innerText = `${hours}:${minutes}:${seconds}  ${ampm}`;
+
+}
+
+function todaysDate(){
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
+
+    document.getElementById("date").innerText = `${month}, ${day}, ${year}`;
+}
+window.onload = todaysDate
+window.onload = time
